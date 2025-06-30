d1d9c747da6217586c455cdc1802aad84142cd5e
juninho877 <alessandrosandrinho877@gmail.com>
2025-06-29 22:54:03 -0300

    Update mercadopago_master.php

--- admin/mercadopago_master.php
+++ admin/mercadopago_master.php
@@ -71,7 +71,7 @@ include "includes/header.php";
     <p class="page-subtitle">Configure a integração com o Mercado Pago para seus usuários</p>
 </div>
 
-<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
     <!-- Formulário de Configuração -->
     <div class="lg:col-span-2">
         <div class="card">
